1c312f0eebad01db2b12e4fcd7a75da2e9ff2f38
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-12 19:21:22 +0600

    addad the update

--- shortHand.js
+++ shortHand.js
@@ -1,3 +1,12 @@
 var price = 35;
 price +=10;
 console.log(price);
+
+var prOne = 40;
+prOne -=10;
+console.log(prOne);
+
+var priceTwo = 40;
+var priceThre = 5;
+var pTwo = priceTwo * priceThre;
+console.log(pTwo);
