b6869cd209df158bfa5b603b73607e65cb8e45b4
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-14 11:06:52 +0600

    addad the multiple file

---
+++ multiple.js
@@ -0,0 +1,9 @@
+var salary = 25000;
+var height = 56;
+var isBcs = true;
+if(salary > 24000 && height > 70 || isBcs == true){
+    console.log('baba noman bolo kobulo');
+}
+else{
+    console.log('vag beta mukbul');
+}
