12dd25b9bd108f06ff4df1e54cd093cd1a8b0bec
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-14 07:27:32 +0600

    addad the if file

---
+++ if.js
@@ -0,0 +1,16 @@
+if(10 < 15){
+    console.log('i have a smaller value');
+}
+
+var biraniPrice = 300;
+if(biraniPrice < 500){
+    console.log('mama give me some bariyani');
+}
+
+var weight = 40;
+if(weight < 20){
+    console.log('I will cary it by my self');
+}
+else{
+    console.log('i will rent a rikshaw');
+}
