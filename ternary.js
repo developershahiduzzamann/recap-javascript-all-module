d2718c8a7bb7cb209a9634196454613c30a85bfd
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-18 13:15:35 +0600

    addad the modify file

--- ternary.js
+++ ternary.js
@@ -1,3 +1,17 @@
 const age = 12;
 age >= 18 ? console.log('vot Dio') : console.log('vot dite hobe na');
-console.log(age);
+
+
+let price = 500;
+const isLeader = true;
+if(isLeader === false){
+    price = 0;
+}
+else{
+    price = price + 100;
+}
+console.log(price);
+
+price = isLeader === true? 0 :price =price+200;
+console.log(price);
+
