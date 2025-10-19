81a25544bd90515049a856bc3d976a9e790210fe
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-19 21:08:48 +0600

    addad the code

--- shortcut.js
+++ shortcut.js
@@ -1,7 +1,17 @@
-const isPased = false;
-if(isPased == false){
-    console.log('toke biya diye dibo');
+// const isPased = false;
+// if(isPased == false){
+//     console.log('toke biya diye dibo');
+// }
+// else{
+//     console.log('toke biye dimo na');
+// }
+
+
+const isPased = true;
+// if(isPased === false)
+if(isPased){
+    console.log('toke diye kichu hobe na');
 }
 else{
-    console.log('toke biye dimo na');
-}
+    console.log('tor biye hobe')
+}  
