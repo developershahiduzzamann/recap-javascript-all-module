b5c30b2d6953e54e322de2f9f53b31cfe0adb40d
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-21 23:10:45 +0600

    addad the all file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const numbers = [1, 34, 24, 6, 66, 898];
+console.log(numbers)
 const fourth = numbers[3];
+numbers[2]=99;
+console.log(numbers);
 console.log(fourth);
 console.log(numbers[1]);
---
+++ push.js
@@ -0,0 +1,4 @@
+const numbers = [1, 34, 24, 6, 66, 898];
+console.log(numbers);
+numbers.push(88);
+console.log(numbers);
