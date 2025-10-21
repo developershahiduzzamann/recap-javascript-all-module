7361e7d4ad5005ffd68f28738652420333b57c20
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-21 06:42:47 +0600

    addad the index file

---
+++ index.js
@@ -0,0 +1,4 @@
+const numbers = [1, 34, 24, 6, 66, 898];
+const fourth = numbers[3];
+console.log(fourth);
+console.log(numbers[1]);
---
+++ length.js
@@ -0,0 +1,2 @@
+const numbers = [1, 34, 24, 6, 66, 898];
+console.log(numbers.length);
