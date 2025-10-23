11073fef0761b7e7d70352eedfdcb94925c74c92
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-23 06:54:35 +0600

    addad the includes file

---
+++ includes.js
@@ -0,0 +1,8 @@
+const numbers = [1, 34, 24, 6, 66, 898];
+console.log(numbers.includes(66))
+if(numbers.includes(14)){
+    console.log('this true number');
+}
+else{
+    console.log('this false number');
+}
---
+++ indexOf.js
@@ -0,0 +1,3 @@
+const numbers = [1, 34, 24, 6, 66, 898];
+console.log(numbers.indexOf(6));
+ 
