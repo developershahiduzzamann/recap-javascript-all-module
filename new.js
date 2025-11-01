919b2ce489fdf6b7696e4476c0ac995a7c6aca02
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-11-01 21:55:41 +0600

    addad the new file

---
+++ new.js
@@ -0,0 +1,7 @@
+//The girl's father's conditions for marriage. age =25, or salary =35000k You can get married.Otherwise You are a child.You can't get married.
+const my_age = 22;
+const marriage_age = 25;
+const salary = 35000;
+const my_salary = 0;
+my_age >= marriage_age || my_salary >=salary? console.log("You can get married.") : console.log("You are a child.You can't get married.");
+//Run JavaScript code.
