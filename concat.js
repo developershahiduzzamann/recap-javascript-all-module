a4566ba3ebcdeaf57e534eec81f91bfdbf584625
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-11-03 20:39:33 +0600

    addad the file

---
+++ concat.js
@@ -0,0 +1,6 @@
+const sentences = 'i am good student for good work';
+let reverse = ''
+for(sentence of sentences){
+    reverse = sentence + reverse;
+    console.log(reverse);
+}
---
+++ slice.js
@@ -0,0 +1,6 @@
+const address = 'AmarDeshBangladesh';
+const part = address.slice(4,8);
+console.log(part);
+
+const sentence = 'i am a good boy ';
+console.log(sentence.split(' '));
